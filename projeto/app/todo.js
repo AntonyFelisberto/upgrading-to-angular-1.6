e6e868f28e04671fe9ceb194791fd98f40d22073
Antony dos santos felisberto <88542984+AntonyFelisberto@users.noreply.github.com>
2023-02-25 23:06:26 -0300

    definindo modelo para usar o binding por variavel

--- projeto/app/todo.js
+++ projeto/app/todo.js
@@ -1,16 +1,17 @@
 angular.module('app').controller('TodoCtrl', function($scope, $timeout) {
   var wayToGoPromise = undefined;
+  var self = this;
 
   $scope.$watch('todo', function() {
-    var name = $scope.todo.name;
-    $scope.formattedTodoName = name.charAt(0).toUpperCase()
+    var name = self.todo.name;
+    self.formattedTodoName = name.charAt(0).toUpperCase()
       + name.substring(1).toLowerCase();
   });
 
-  $scope.$watch('todo.done', displayWayToGoMessage);
+  $scope.$watch(()=> {return self.todo.done;}, displayWayToGoMessage);
 
   function displayWayToGoMessage() {
-    if ($scope.todo.done) {
+    if (self.todo.done) {
       $scope.showWayToGo = true;
 
       if (wayToGoPromise) {
@@ -18,7 +19,7 @@ angular.module('app').controller('TodoCtrl', function($scope, $timeout) {
       }
 
       wayToGoPromise = $timeout(function() {
-        $scope.showWayToGo = false;
+        self.showWayToGo = false;
       }, 1500);
     }
   }
@@ -32,6 +33,8 @@ angular.module('app').directive('todo', function() {
       todo:'<',
       deleteTodo:'&'
     },
-    controller: 'TodoCtrl'
+    controller: 'TodoCtrl',
+    controllerAs:'$ctrl',
+    bindToController:true
   };
 });
